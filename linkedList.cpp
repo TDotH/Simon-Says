a5b6f4512bb650a2219f6abed4debdabbc1e72cc
Tyde Hashimoto <hasht795@hotmaill.com>
2019-10-15 14:20:11 -0700

    Creation of list structure

---
+++ linkedList.cpp
@@ -0,0 +1,89 @@
+#include "linkedList.h"
+
+//Constructor
+list::list() {
+
+    head = nullptr;
+    size = 0;
+}
+
+//Destructor
+list::~list() {
+
+    deleteList( head );
+}
+
+/* Insert ( public )
+*  Param: val to be inserted
+*/
+
+void list::insert( int val ) {
+
+    insert( val, head );
+    return;
+}
+
+/* Insert ( private )
+*  Param: val to be inserted, a node
+*/
+
+void list::insert( int val, node *& head ) {
+
+    if ( head ) {
+
+        insert( val, head->next );
+    } else {
+
+        node * aNode = new node( val );
+        head = aNode;
+    }
+    return;
+}
+
+/* getValAt ( public )
+*   -Param:  which node val to be returned
+*   -Return: node val
+*/
+
+int list::getValAt( int place ) {
+
+    int returnVal = getValAt( place, head );
+    return returnVal;
+}
+
+/* getValAt ( private )
+*   -Param:  which node val to be returned, a node
+*   -Return: node val
+*/
+int list::getValAt( int place, node * head ) {
+
+    int returnVal = 0;
+
+    //Found the node and node exists
+    if ( ( place == 0 ) && head ) {
+
+        returnVal = head->val;
+    } else {
+
+        returnVal = getValAt( place - 1, head->next );
+    }
+
+    return returnVal;
+}
+
+/* deleteList ( private )
+*   -Param: a node
+*/
+
+void list::deleteList( node *& head ) {
+
+    if ( head ) {
+
+        node * temp = head->next;
+        delete head;
+        head = nullptr;
+        deleteList( temp );
+    }
+
+    return;
+}
--- linkedList.h
+++ linkedList.h
@@ -1,1 +1,45 @@
-//Singly linked list data structure
+/*Singly linked list data structure
+*   -Holds the list itself
+*/
+
+#ifndef LIST_H
+#define LIST_H
+
+class list {
+
+  public:
+
+  list();
+  ~list();
+
+  void insert( int val );
+
+  int getSize() { return size; };
+  int getValAt( int place );
+
+  private:
+
+    struct node;
+
+    //Members
+    node * head;
+    int size;
+
+    //Functions
+    void insert( int val, node *& head );
+    void deleteList( node *& head );
+
+    int getValAt( int place, node * head );
+
+};
+
+struct list::node {
+
+  node * next;
+  int val;
+
+  node( int x ): val( x ), next( nullptr ) {};
+
+};
+
+#endif
