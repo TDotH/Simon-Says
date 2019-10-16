abf07ef58e01504cf856bac4d3a1c46aba2eb3e2
Tyde Hashimoto <hasht795@hotmaill.com>
2019-10-16 00:12:43 -0700

    More ignored files; Added SDL utility files

---
+++ SDLUtil.cpp
@@ -0,0 +1,75 @@
+#include "SDLUtil.h"
+
+
+//Initializes SDL
+SDL_Window * initSDL() {
+
+    int width  = 800;
+    int height = 600;
+
+    const char * title = "Simon Says v1.0";
+
+
+
+    SDL_Window * mainWindow = NULL;
+
+    //Initialize SDL
+    if ( SDL_Init( SDL_INIT_VIDEO ) != 0 ) {
+
+        printf( "Unable to initalize SDL: %s", SDL_GetError() );
+    } else {
+
+        //Create Window
+        mainWindow = SDL_CreateWindow( title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, height, width, 0 );
+
+        //Load support for PNG image format
+        int imgFlags = IMG_INIT_PNG;
+        if ( !( IMG_Init( imgFlags ) & imgFlags ) ) {
+
+                printf( "SDL_image could not initialize! Error: %s\n", IMG_GetError() );
+        }
+    }
+
+    return mainWindow;
+}
+
+//Loads an optimized image
+SDL_Surface * loadImage( const char * imageLoc, SDL_Surface * mainSurface ) {
+
+    //Final optimized image
+    SDL_Surface * optImage = NULL;
+
+    SDL_Surface * loadedImage = IMG_Load( imageLoc );
+    if ( !loadedImage ) {
+
+        printf( "Unable to load image %s! Error: %s\n", imageLoc, IMG_GetError() );
+    } else {
+
+        //Convert image to screen format
+        optImage = SDL_ConvertSurface( loadedImage, mainSurface->format, 0);
+
+        if ( !optImage ) {
+
+                printf( "Unable to optimize image %s! Error: %s\n", imageLoc, IMG_GetError() );
+        }
+
+        //Free previous image
+        SDL_FreeSurface( loadedImage );
+    }
+
+    return optImage;
+}
+
+//Closes SDL
+void closeSDL ( SDL_Window * mainWindow, SDL_Surface * mainSurface ) {
+
+    //Free current surface
+    SDL_FreeSurface( mainSurface );
+    mainSurface = NULL;
+
+    //Free window
+    SDL_DestroyWindow( mainWindow );
+    mainWindow = NULL;
+
+    SDL_Quit();
+}
---
+++ SDLUtil.h
@@ -0,0 +1,19 @@
+//Holds main SDL Utilites
+
+#ifndef SDLUTIL_H
+#define SDLUTIL_H
+
+#include <SDL.h>
+#include <stdio.h>
+#include <SDL_Image.h>
+
+//Initializes SDL
+SDL_Window * initSDL();
+
+//Loads an optimized image
+SDL_Surface * loadImage( const char * imageLoc, SDL_Surface * mainSurface );
+
+//Closes SDL
+void closeSDL ( SDL_Window * mainWindow, SDL_Surface * mainSurface );
+
+#endif
